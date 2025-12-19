1adf161221795bc51aa819b21f11adf7c7e871ca
bmonagan <bernardmonagan@gmail.com>
2025-12-18 20:45:34 -0500

    player class working and main ship is showing on screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, LINE_WIDTH, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 def main():
     print(f'Starting Asteroids with pygame version: {pygame.version.ver}')
     print(f'Screen width: {SCREEN_WIDTH}')
@@ -9,6 +10,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,PLAYER_RADIUS)
+
     while True:
         dt = clock.tick(60) / 1000
         log_state()
@@ -16,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        player.draw(screen,"white",player.triangle(),LINE_WIDTH)
         pygame.display.flip()
 
 
